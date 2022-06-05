e52a16f232e1ae6670dd751730be3e5b320dd759
buschinelli-joao <106926414+buschinelli-joao@users.noreply.github.com>
2022-06-05 17:34:23 -0300

    Game commits
    Some of the cs scripts I'm developing for my game

---
+++ cameraFollowScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowScript : MonoBehaviour
+{
+    public Transform target;
+    public float smoothedFactor;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPosition = target.position - new Vector3(0, 0, 10);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothedFactor * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+    }
+}
